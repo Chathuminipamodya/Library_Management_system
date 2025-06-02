9f0e645a3283cf4750435c271b0d701de3fd1c9c
Chathuminipamodya <138112305+Chathuminipamodya@users.noreply.github.com>
2025-06-02 22:38:54 +0530

    Header and footer structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MaterialModule } from './material/material.module';
+import { SharedModule } from './shared/shared.module';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,MaterialModule],
+  imports: [RouterOutlet,SharedModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
