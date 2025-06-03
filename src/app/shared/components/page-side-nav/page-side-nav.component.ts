cc3212af8cdd1f19f57e51278a2c272eabc82ac8
Chathuminipamodya <138112305+Chathuminipamodya@users.noreply.github.com>
2025-06-03 14:17:19 +0530

    Page sidebar and apge not found created

---
+++ src/app/shared/components/page-side-nav/page-side-nav.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-side-nav',
+  standalone: false,
+  templateUrl: './page-side-nav.component.html',
+  styleUrl: './page-side-nav.component.scss'
+})
+export class PageSideNavComponent {
+  panelName: string ="Student Panel";
+
+}
